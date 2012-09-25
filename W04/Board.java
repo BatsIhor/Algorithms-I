6ab726becd3fc58ca7d2508580e5d4e050afbe4d
ack <ack@Singularity.local>
2012-09-25 00:48:53 -0300

    Adding Week 3 and 4

---
+++ W04/Board.java
@@ -0,0 +1,36 @@
+public class Board {
+    public Board(int[][] blocks) {
+    }
+    
+    public int dimension() {
+        return 0;
+    }
+    
+    public int hamming() {
+        return 0;
+    }
+    
+    public int manhattan() {
+        return 0;
+    }
+    
+    public boolean isGoal() {
+        return false;
+    }
+    
+    public Board twin() {
+        return null;
+    }
+    
+    public boolean equals(Object that) {
+        return false;
+    }
+    
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+    
+    public String toString() {
+        return "";
+    }
+}
